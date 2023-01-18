fb17ecb408cf11d99fcaf5f1577cedb3704cc049
Adrian Irwin <adrian.irwin7@mail.dcu.ie>
2023-01-18 18:46:15 +0000

    load passengers on driver page from backend

--- src/Carpool/components/Driver.js
+++ src/Carpool/components/Driver.js
@@ -1,32 +1,73 @@
-import React from 'react';
-import { Platform, StyleSheet, View,} from 'react-native';
-import { List } from 'react-native-paper';
+import { BACKEND_URL } from "@env";
+import React, { useCallback, useEffect, useState } from 'react';
+import { Platform, RefreshControl, ScrollView, StyleSheet} from 'react-native';
+import { ActivityIndicator, List } from 'react-native-paper';
 
 
 const Driver = () => {
-    return (
-        <View style={styles.container}>
-            <List.Item onPress={() => console.log(`user on ${Platform.OS} pressed Adam's card`)}
-                title="Adam"
-                description="3 people, leaving at 8:00am"
-                descriptionNumberOfLines={2}
-                left={props => <List.Icon {...props} icon="seat-passenger" /> }
-            />
-            <List.Item  onPress={() => console.log(`user on ${Platform.OS} pressed Scott's card`)}
-                title="Scott"
-                description="1 person, leaving at 10:00am"
-                descriptionNumberOfLines={2}
-                left={props => <List.Icon {...props} icon="seat-passenger" />}
-            />
+    const [refreshing, setRefreshing] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [passengers, setPassengers] = useState([]);
 
-        </View>
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        setTimeout(() => {
+            getPassengers();
+            setRefreshing(false);
+        }, 1000);
+    }, [])
+
+    const getPassengers = async () => {
+        try {
+            let response = await fetch(`${BACKEND_URL}/api/getAll`);
+            let json = await response.json();
+            setPassengers(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getPassengers();
+    }, [] );
+
+    return (
+        <ScrollView
+            style={styles.container}
+            refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+        >
+            {isLoading ? <ActivityIndicator animating={true} color="#f452" size="large" /> : (
+                passengers.map((passenger, index) => {
+                    let description = ``;
+                    if (passenger.noOfPassengers === undefined || passenger.noOfPassengers === 1) {passenger.noOfPassengers = 1; description += `${passenger.noOfPassengers} passenger`;}
+                    else description += `${passenger.noOfPassengers} passengers`;
+
+                    description += `, leaving at ${passenger.departureTime}`;
+                    return (
+                        <List.Item
+                            key={index}
+                            onPress={() => console.log(`user on ${Platform.OS} pressed ${passenger.name}'s card`)}
+                            title={passenger.name}
+                            description={description}
+                            descriptionNumberOfLines={2}
+                            left={props => <List.Icon {...props} icon="seat-passenger" />}
+                            style={{marginBottom: 10, borderRadius: 10, borderWidth: .6, borderColor: '#000'}}
+                        />
+                    );
+                })
+            )}
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        margin: 25,
+        padding: 25,
     }
 });
 
