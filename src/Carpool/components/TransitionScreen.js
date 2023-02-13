84969e118d74573f26a3bd38b398e34c261c3d14
Adrian Irwin <adrian.irwin7@mail.dcu.ie>
2023-02-13 23:14:43 +0000

    get and add driverID to currentUser on TransitionScreen open

--- src/Carpool/components/TransitionScreen.js
+++ src/Carpool/components/TransitionScreen.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import TransitionCard from './TransitionCard';
+import { CurrentUserContext } from "./Context";
+import { BACKEND_URL } from "@env";
 
 const TransitionScreen = ({ navigation }) => {
+    const [currentUser, setCurrentUser] = React.useContext(CurrentUserContext);
+
+    const getDriverID = () => {
+        fetch(`${BACKEND_URL}/api/driver/getDriverID/${currentUser.userID}`)
+            .then((response) => {
+                if (response.ok) {
+                    response.json().then((data) => {
+                        console.log("driverID =", data);
+                        setCurrentUser({ ...currentUser, driverID: data });
+                    })
+                    .catch((error) => {
+                        console.log("Error getting driverID", error);
+                    });
+                }
+            })
+            .catch((error) => {
+                console.log("Error getting driverID", error);
+            });
+    }
+
+    useEffect(() => {
+        getDriverID();
+    }, [currentUser.userID])
+
     return (
         <View style={styles.container}>
-            <TransitionCard title="Driver" description="DriverHomePage" navigation={navigation}/>
-            <TransitionCard title="Passenger" description= "PassengerHomePage" navigation={navigation}/>
+            <TransitionCard title="Driver" description="DriverHomePage" navigation={navigation} />
+            <TransitionCard title="Passenger" description="PassengerHomePage" navigation={navigation} />
         </View>
     );
 };
