a0d6194d9cdc703207360935c78dd370720afa38
Adrian Irwin <adrian.irwin7@mail.dcu.ie>
2023-02-13 20:57:26 +0000

    created function to add passenger to drivers accepted passengers array

--- src/Carpool/components/Driver/PassengerInfo.js
+++ src/Carpool/components/Driver/PassengerInfo.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Avatar, Button, Card, Text } from 'react-native-paper';
-import { StyleSheet, View} from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import MapView, { Marker } from 'react-native-maps';
+import {  CurrentUserContext } from "../Context";
+import { BACKEND_URL } from "@env";
 
-const PassengerInfo = ({route, navigation}) => {
+const PassengerInfo = ({ route, navigation }) => {
+    const [currentUser] = useContext(CurrentUserContext);
     const passenger = route.params.passenger;
-    console.log(passenger)
+
+    const sendToDriver = () => {
+        fetch(`${BACKEND_URL}/api/driver/addPassenger/${currentUser.driverID}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json", },
+            body: JSON.stringify({ passenger: passenger }),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    response.json().then((data) => {
+                        console.log("ok response", JSON.stringify(data))
+                        navigation.navigate('DriverHomePage', { passenger });
+                    });
+                    console.log("Passenger added to driver");
+                } else {
+                    response.json().then((data) => { console.log("non ok response", data); });
+                    console.log("Error adding passenger to driver");
+                }
+            })
+            .catch((error) => {
+                console.log(error, "Error adding passenger to driver");
+            });
+    };
+
 
     let description = ``;
-    if (passenger.noOfPassengers === undefined || passenger.noOfPassengers === 1) {passenger.noOfPassengers = 1; description += `there is one person on this ride`;}
-    else {description += `There are ${passenger.noOfPassengers} people on this ride.`;}
+    if (passenger.noOfPassengers === undefined || passenger.noOfPassengers === 1) { passenger.noOfPassengers = 1; description += `there is one person on this ride`; }
+    else { description += `There are ${passenger.noOfPassengers} people on this ride.`; }
     return (
         <View style={styles.card}>
-            <Card style= {styles.card}>
-                <Card.Content style={{alignItems: "center"}}>
-                    <Avatar.Icon icon="account" size={100}/>
+            <Card style={styles.card}>
+                <Card.Content style={{ alignItems: "center" }}>
+                    <Avatar.Icon icon="account" size={100} />
                 </Card.Content>
-                    <Text style={styles.title}>{passenger.name}</Text>
+                <Text style={styles.title}>{passenger.name}</Text>
                 <Card.Content>
                     <MapView
                         provider='google'
-                        style={{height: 200, borderRadius: 20, marginBottom: 10}}
+                        style={{ height: 200, borderRadius: 20, marginBottom: 10 }}
                         initialRegion={{
                             latitude: passenger.location.latitude,
                             longitude: passenger.location.longitude,
@@ -34,7 +60,7 @@ const PassengerInfo = ({route, navigation}) => {
                                 longitude: passenger.location.longitude,
                             }}
                             title="Pickup Location"
-                            description="DCU"
+                            description="User's pickup location"
                         />
                     </MapView>
                 </Card.Content>
@@ -42,9 +68,11 @@ const PassengerInfo = ({route, navigation}) => {
                     <Text style={styles.info}>{passenger.name} will be leaving at {passenger.departureTime}</Text>
                     <Text style={styles.info}>{description}</Text>
                 </Card.Content>
-                    <Button style={styles.button} mode="contained" onPress={() =>{navigation.navigate('DriverHomePage', {passenger});
-                        console.log(`Driver accepted ${passenger.name}'s ride`)}}contentStyle={{padding: 10}}>Click here to accept this ride</Button>
-                </Card>
+                <Button style={styles.button} mode="contained" onPress={() => {
+                    console.log(`Driver accepted ${passenger.name}'s ride`)
+                    sendToDriver();
+                }} contentStyle={{ padding: 10 }}>Click here to accept this ride</Button>
+            </Card>
         </View>
     )
 }
@@ -72,7 +100,7 @@ const styles = StyleSheet.create({
         fontSize: 20,
         textAlign: 'center',
         fontStyle: 'italic',
-        },
+    },
     button: {
         borderRadius: 30,
         marginTop: 20,
